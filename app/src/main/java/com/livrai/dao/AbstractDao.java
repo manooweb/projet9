0fb3465be6da539d729270427c18fd444a794845
Emmanuel Hesry <ehesry@gmail.com>
2026-09-15 19:12:48 +0200

    Externalize DB connexion data to be able to start app in Docker environment without modifying code

--- app/src/main/java/com/livrai/dao/AbstractDao.java
+++ app/src/main/java/com/livrai/dao/AbstractDao.java
@@ -6,6 +6,12 @@ import java.sql.SQLException;
 
 public class AbstractDao {
 
+    private static final String DEFAULT_DATABASE_HOST = "localhost";
+    private static final String DEFAULT_DATABASE_PORT = "3306";
+    private static final String DEFAULT_DATABASE_NAME = "livrai";
+    private static final String DEFAULT_DATABASE_USER = "root";
+    private static final String DEFAULT_DATABASE_PASSWORD = "password";
+
     private Connection connection;
 
     private Connection getConnection() {
@@ -30,10 +36,22 @@ public class AbstractDao {
         }
 
         try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/livrai", "root", "password");
+            String host = getEnvironmentVariable("LIVRAI_DB_HOST", DEFAULT_DATABASE_HOST);
+            String port = getEnvironmentVariable("LIVRAI_DB_PORT", DEFAULT_DATABASE_PORT);
+            String database = getEnvironmentVariable("LIVRAI_DB_NAME", DEFAULT_DATABASE_NAME);
+            String user = getEnvironmentVariable("LIVRAI_DB_USER", DEFAULT_DATABASE_USER);
+            String password = getEnvironmentVariable("LIVRAI_DB_PASSWORD", DEFAULT_DATABASE_PASSWORD);
+
+            connection = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + database, user,
+                    password);
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    private String getEnvironmentVariable(String name, String defaultValue) {
+        String value = System.getenv(name);
+        return value == null || value.isEmpty() ? defaultValue : value;
     }
 
     protected interface SqlQuery<T> {
